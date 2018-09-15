2c42f82930dd11cbb5feb07544c48abecace3f62
rahulyerva <42656632+rahulyerva@users.noreply.github.com>
2018-09-14 21:52:10 -0500

    ICP-4_Assignment

---
+++ TranslateApp/app/src/main/java/tutorial/cs5551/com/translateapp/TranslateActivity.java
@@ -0,0 +1,130 @@
+package tutorial.cs5551.com.translateapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class TranslateActivity extends AppCompatActivity implements OnItemSelectedListener {
+
+    String API_URL = "https://api.fullcontact.com/v2/person.json?";
+    String API_KEY = "b29103a702edd6a";
+    String sourceText;
+    TextView outputTextView;
+    Context mContext;
+    HashMap<String,String> language=new HashMap<String,String>();
+    String inputword,langcode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translate);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        outputTextView = (TextView) findViewById(R.id.txt_Result);
+        //Adding Key , value pairs as below
+        language.put("Telugu","te");
+        language.put("Tamil","ta");
+        language.put("Spanish","es");
+        language.put("urdu","ur");
+        language.put("Chinese","zh");
+        language.put("Czech","cs");
+        language.put("Kanada","kn");
+        language.put("Japanese","ja");
+
+        List<String> ls=new ArrayList<String>(language.keySet());
+        Spinner spinner=(Spinner)findViewById(R.id.spinner2);
+        ArrayAdapter<String> languageadapter=new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,ls);
+        languageadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(languageadapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    public void translateText(View v) {
+        TextView sourceTextView = (TextView) findViewById(R.id.txt_Email);
+
+        sourceText = sourceTextView.getText().toString();
+        String getURL = "https://translate.yandex.net/api/v1.5/tr.json/translate?" +
+                "key=trnsl.1.1.20151023T145251Z.bf1ca7097253ff7e." +
+                "c0b0a88bea31ba51f72504cc0cc42cf891ed90d2&text=" + sourceText +"&" +
+                "lang=en-"+langcode+"&[format=plain]&[options=1]&[callback=set]";//The API service URL
+        final String response1 = "";
+        OkHttpClient client = new OkHttpClient();
+        try {
+            Request request = new Request.Builder()
+                    .url(getURL)
+                    .build();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    System.out.println(e.getMessage());
+                }
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    final JSONObject jsonResult;
+                    final String result = response.body().string();
+                    try {
+                        jsonResult = new JSONObject(result);
+                        JSONArray convertedTextArray = jsonResult.getJSONArray("text");
+                        final String convertedText = convertedTextArray.get(0).toString();
+                        Log.d("okHttp", jsonResult.toString());
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                               Log.d("check",convertedText);
+                                outputTextView.setText(convertedText);
+                            }
+                        });
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+
+        } catch (Exception ex) {
+            outputTextView.setText(ex.getMessage());
+
+        }
+
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        inputword=adapterView.getItemAtPosition(i).toString();
+        Log.d("language",inputword);
+        langcode=language.get(inputword).toString();
+        Log.d("code",langcode);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
